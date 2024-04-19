adfbd2e056afb665258e1b73c8523fd1f2be0195
unknown <alek.cv.c@gmail.com>
2024-04-19 23:59:30 +0200

    dodati podaci

---
+++ cvs to mongo.py
@@ -0,0 +1,34 @@
+import csv
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://localhost:27017/')
+
+db = client['sobadb']
+
+collection = db['senzor']
+
+csv_file_path = 'C:\\Users\\Aleksandar\\Desktop\\iot_telemetry_data.csv'
+
+def import_data_from_csv(csv_file_path, collection):
+    with open(csv_file_path, 'r',encoding='utf-8-sig') as file:
+        reader = csv.DictReader(file)
+        cnt = 0
+        for row in reader:
+            #print(row)
+            cnt +=1
+            data = {
+                'timestamp': row['ts'],
+                'device': row['device'],
+                'carbonMonoxide': row['co'],
+                'humidity': row['humidity'],
+                'light': row['light'],
+                'motion': row['motion'],
+                'smoke': row['smoke'],
+                'temp': row['temp']
+            }
+            if cnt == 5000:
+                break
+            collection.insert_one(data)
+    print("Data imported successfully from CSV to MongoDB!")
+
+import_data_from_csv(csv_file_path, collection)
